d5d24e02704331f9dbedf42c92b1c0e2eba9610e
David <davidlpzgomez@gmail.com>
2013-06-15 01:52:47 +0200

    Pages commit

---
+++ demo/settings.php
@@ -0,0 +1,47 @@
+<?php 
+
+//SETTINGS FILE
+
+$settings['version'] = 'v0.22';
+
+// Page title
+$settings['title'] = 'Carrots';
+
+// Folder's path
+$settings['path'] = 'images';
+
+// Theme to display
+$settings['theme'] = 'centered.css';
+
+// Order of the navegation menu
+// Values: ['ASC' | 'DESC']
+$settings['order_menu'] = 'ASC';
+
+// Filetypes that can will be display
+$settings['extensions'] = array();
+$settings['extensions'][] = 'png';
+$settings['extensions'][] = 'jpg';
+$settings['extensions'][] = 'jpeg';
+$settings['extensions'][] = 'gif';
+
+// Display the folders menu 
+// Values: [ true | false ]
+$settings['show_menu'] = true;
+
+// Display the folders grid at the home page 
+// Values: [ true | false ]
+$settings['show_covers_grid'] = true;
+
+// Display the cover inside the gallery 
+// Values: [ true | false ]
+$settings['show_cover'] = false;
+
+// Order of the files
+// Values: ['ASC' | 'DESC']
+$settings['order_files'] = 'ASC';
+
+// Display images in grid or list mode 
+// Values: [ 'grid' | 'list' ]
+$settings['display_mode'] = 'list';
+	
+?>
